9e776984c1937e51acc10b23cdb096fc40c8d3cf
Matthew_Cash <matthew@matthew-cash.com>
2022-03-26 20:49:29 -0700

    Use symlink for keyrings dir

--- home/matthew/applications/gnome.nix
+++ home/matthew/applications/gnome.nix
@@ -1,40 +1,48 @@
 { pkgs, persistenceHomePath, name, ... }:
 
 {
-    home.persistence."${persistenceHomePath}/${name}".directories = [
-        ".local/share/gnome-podcasts"
-        ".local/share/gnome-boxes"
-        ".local/share/gnome-photos"
-        ".local/share/gnome-settings-daemon"
-        ".local/share/gnome-shell"
-        ".local/share/org.gnome.SoundRecorder"
-        ".local/share/totem"
-        ".local/share/gvfs-metadata"
-        ".local/share/flatpak"
-        ".local/share/backgrounds"
-        ".local/share/keyrings"
-        ".local/share/nautilus"
-        ".local/share/gnome-podcasts"
+    # TODO: move persistence option to move relevant config files
 
-        "./config/fragments"
-        "./config/totem"
-        "./config/gnome-control-center"
-        "./config/gnome-boxes"
-        "./config/gnome-session"
-        "./config/eog"
+    home.persistence."${persistenceHomePath}/${name}" = {
+        directories = [
+            ".local/share/gnome-podcasts"
+            ".local/share/gnome-boxes"
+            ".local/share/gnome-photos"
+            ".local/share/gnome-settings-daemon"
+            ".local/share/gnome-shell"
+            ".local/share/org.gnome.SoundRecorder"
+            ".local/share/totem"
+            ".local/share/gvfs-metadata"
+            ".local/share/flatpak"
+            ".local/share/backgrounds"
+            ".local/share/nautilus"
+            ".local/share/gnome-podcasts"
 
-        ".cache/gnome-boxes"
-        ".cache/gnome-calculator"
-        ".cache/gnome-calendar"
-        ".cache/gnome-desktop-thumbnailer"
-        ".cache/gnome-podcasts"
-        ".cache/gnome-screenshot"
-        ".cache/totem"
-        ".cache/thumbnails"
-        ".cache/org.gnome.Books"
-        ".cache/org.gnome.SoundRecorder"
-        ".cache/"
-    ];
+            ".config/fragments"
+            ".config/totem"
+            ".config/gnome-control-center"
+            ".config/gnome-boxes"
+            ".config/gnome-session"
+            ".config/eog"
+
+            ".cache/gnome-boxes"
+            ".cache/gnome-calculator"
+            ".cache/gnome-calendar"
+            ".cache/gnome-desktop-thumbnailer"
+            ".cache/gnome-podcasts"
+            ".cache/gnome-screenshot"
+            ".cache/totem"
+            ".cache/thumbnails"
+            ".cache/org.gnome.Books"
+            ".cache/org.gnome.SoundRecorder"
+            ".cache/"
+        ];
+        files = [
+            # gnome-keyring-daemon misbehaves in bindfs, symlinks are fine
+            # seems to be similar to https://gitlab.gnome.org/GNOME/gnome-keyring/-/issues/84
+            ".local/share/keyrings"
+        ];
+    };
 
     home.packages = with pkgs; with gnome; [
         eog
