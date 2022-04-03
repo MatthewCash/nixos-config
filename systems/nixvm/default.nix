71150a7a8a6c3d6391bd5d70b89d419bd752c84b
Matthew_Cash <matthew@matthew-cash.com>
2022-04-03 12:55:33 -0700

    Update nixvm kernel to 5.17

--- systems/nixvm/default.nix
+++ systems/nixvm/default.nix
@@ -4,7 +4,7 @@
     systemConfig = rec {
         hostname = "nixvm";
         system = "x86_64-linux";
-        kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_16;
+        kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_17;
 
         nixosConfig = builtins.toString ./config;
         homeConfig = builtins.toString ./home;
