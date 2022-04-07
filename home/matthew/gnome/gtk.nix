265c2214aa44da19fb4a444e2b768d0fbe15ce18
Matthew_Cash <matthew@matthew-cash.com>
2022-04-07 12:25:23 -0700

    Update kora icons and gtk3 theme

--- home/matthew/gnome/gtk.nix
+++ home/matthew/gnome/gtk.nix
@@ -1,16 +1,29 @@
 { pkgs, ... }:
 
+let
+    kora-icon-theme = pkgs.kora-icon-theme.overrideAttrs(oldAttrs: {
+        src = pkgs.fetchFromGitHub {
+            owner = "bikass";
+            repo = "kora";
+            rev = "af092d2e99b4fb8f83f37bf8ddc5acae43bb36dc";
+            sha256 = "sha256-uNQlKginkEim5kRX/aesKyvGyCb/wDRD2GCjJsOMRB4=";
+        };
+    });
+
+    adw-gtk3-theme = pkgs.callPackage ./adw-gtk3-theme.nix { };
+in
+
 {   
     gtk = {
         enable = true;
 
         theme = { 
-            package = pkgs.orchis-theme;
-            name = "Orchis-dark";
+            package = adw-gtk3-theme;
+            name = "adw-gtk3-dark";
         };
 
         iconTheme = {
-            package = pkgs.kora-icon-theme;
+            package = kora-icon-theme;
             name = "kora";
         };
 
