898f6ae34661c73c9f13beec6656239fe5a101e8
Matthew_Cash <matthew@matthew-cash.com>
2022-04-19 12:28:16 -0700

    Bump zenbook kernel 5.16 -> 5.17

--- systems/zenbook/default.nix
+++ systems/zenbook/default.nix
@@ -4,7 +4,7 @@
     systemConfig = rec {
         hostname = "zenbook";
         system = "x86_64-linux";
-        kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_16;
+        kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_17;
         ssd = true;
         vpnAddress = "10.0.0.9";
 
