7934e3311b0882cc140767a476b80e3dcd15b42f
Matthew_Cash <matthew@matthew-cash.com>
2022-05-06 13:12:08 -0700

    Add comments for acpid commands

--- config/acpi.nix
+++ config/acpi.nix
@@ -5,12 +5,15 @@
         enable = true;
 
         lidEventCommands = ''
+            # If the lid is closed and charger is unplugged, suspend after 15 seconds
             [[ `cat /proc/acpi/button/lid/LID/state | ${pkgs.gawk}/bin/awk '{print $2}'` == "closed" ]] && [[ `cat /sys/class/power_supply/BAT0/status` == "Discharging" ]] && sleep 15 && [[ `cat /sys/class/power_supply/BAT0/status` == "Discharging" ]] && [[ `cat /proc/acpi/button/lid/LID/state | ${pkgs.gawk}/bin/awk '{print $2}'` == "closed" ]] && systemctl suspend
         '';
 
         acEventCommands = ''
+            # If the lid is closed and charger is unplugged, suspend after 15 seconds
             sleep 1 && [[ `cat /sys/class/power_supply/BAT0/status` == "Discharging" ]] && [[ `cat /proc/acpi/button/lid/LID/state | ${pkgs.gawk}/bin/awk '{print $2}'` == "closed" ]] && sleep 5 && [[ `cat /sys/class/power_supply/BAT0/status` == "Discharging" ]] && [[ `cat /proc/acpi/button/lid/LID/state | ${pkgs.gawk}/bin/awk '{print $2}'` == "closed" ]] && systemctl suspend
 
+            # Set battery limit to 80% when plugged in
             [[ `cat /sys/class/power_supply/BAT0/status` == "Charging" ]] && echo 81 > /sys/class/power_supply/BAT0/charge_control_end_threshold
         '';
     };
