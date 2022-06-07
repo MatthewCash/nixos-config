2fbad5ce69f600687ae8f121bfe47b403503ed12
Matthew_Cash <matthew@matthew-cash.com>
2022-06-07 12:25:30 -0700

    Add rust toolchain

--- home/matthew/applications/dev.nix
+++ home/matthew/applications/dev.nix
@@ -6,5 +6,14 @@
         gdb
         gnumake
         binutils
+
+        # Rust
+        cargo
+        rustc
+        rustfmt
     ];
+
+    home.sessionVariables = {
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
 }
