685519c0fdf9927089505c16c94315eca0eadd0d
Matthew_Cash <matthew@matthew-cash.com>
2022-06-10 13:46:25 -0700

    nvim-coc: use package from nixpkgs

--- home/matthew/applications/neovim.nix
+++ home/matthew/applications/neovim.nix
@@ -11,16 +11,6 @@
         
         coc = {
             enable = true;
-            package = pkgs.vimUtils.buildVimPluginFrom2Nix {
-                pname = "coc.nvim";
-                version = "2022-05-21";
-                src = pkgs.fetchFromGitHub {
-                    owner = "neoclide";
-                    repo = "coc.nvim";
-                    rev = "791c9f673b882768486450e73d8bda10e391401d";
-                    sha256 = "sha256-MobgwhFQ1Ld7pFknsurSFAsN5v+vGbEFojTAYD/kI9c=";
-                };
-            };
             settings = {
                 suggest = {
                     noselect = false;
