1b46fbfa1d1461603a00349c7552a504589a6411
Matthew_Cash <matthew@matthew-cash.com>
2022-06-20 13:20:21 -0700

    zenbook: linux kernel 5.18

--- systems/zenbook/default.nix
+++ systems/zenbook/default.nix
@@ -4,7 +4,7 @@
     systemConfig = rec {
         hostname = "zenbook";
         system = "x86_64-linux";
-        kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_17;
+        kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_18;
         ssd = true;
         vpnAddress = "10.0.0.9";
 
