09515251aa21db1dabbf79bbf1ddc2a198442438
Matthew_Cash <matthew@matthew-cash.com>
2022-06-30 19:12:28 -0700

    kora-icons: use package from nixpkgs

--- home/matthew/gnome/gtk.nix
+++ home/matthew/gnome/gtk.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }:
 
 let
-    kora-icon-theme = pkgs.kora-icon-theme.overrideAttrs(oldAttrs: {
-        src = pkgs.fetchFromGitHub {
-            owner = "bikass";
-            repo = "kora";
-            rev = "af092d2e99b4fb8f83f37bf8ddc5acae43bb36dc";
-            sha256 = "sha256-uNQlKginkEim5kRX/aesKyvGyCb/wDRD2GCjJsOMRB4=";
-        };
-    });
-
     adw-gtk3-theme = pkgs.callPackage ./adw-gtk3-theme.nix { };
 in
 
@@ -23,7 +14,7 @@ in
         };
 
         iconTheme = {
-            package = kora-icon-theme;
+            package = pkgs.kora-icon-theme;
             name = "kora";
         };
 
