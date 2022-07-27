85e9ffbed69dcde6fdb99063b17c40fb994ca0bd
Matthew_Cash <matthew@matthew-cash.com>
2022-07-26 17:41:37 -0700

    nixvm: add helix module

--- systems/nixvm/home/matthew/default.nix
+++ systems/nixvm/home/matthew/default.nix
@@ -14,4 +14,5 @@
     ../../../../home/${homeName}/applications/vscode-server.nix
     ../../../../home/${homeName}/applications/nix.nix
     ../../../../home/${homeName}/applications/direnv.nix
+    ../../../../home/${homeName}/applications/helix.nix
 ]
