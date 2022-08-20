6447743014f10134ca1985dbdeb4e0540942891f
Matthew_Cash <matthew@matthew-cash.com>
2022-08-20 14:19:06 -0700

    nixvm: linux kernel 5.19

--- systems/nixvm/default.nix
+++ systems/nixvm/default.nix
@@ -3,7 +3,7 @@
 rec {
     hostname = "nixvm";
     system = "x86_64-linux";
-    kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_18;
+    kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_19;
 
     nixosConfig = builtins.toString ./config;
     homeConfig = builtins.toString ./home;
