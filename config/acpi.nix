757753c7727e1471fe6a30ac719af004f793eac1
Matthew_Cash <matthew@matthew-cash.com>
2022-09-20 13:41:14 -0700

    acpi: use variable to store battery target

--- config/acpi.nix
+++ config/acpi.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 
+let batteryTarget = 80; in
 {
     services.acpid = {
         enable = true;
@@ -14,7 +15,7 @@
             sleep 1 && [[ `cat /sys/class/power_supply/BAT0/status` == "Discharging" ]] && [[ `cat /proc/acpi/button/lid/LID/state | ${pkgs.gawk}/bin/awk '{print $2}'` == "closed" ]] && sleep 5 && [[ `cat /sys/class/power_supply/BAT0/status` == "Discharging" ]] && [[ `cat /proc/acpi/button/lid/LID/state | ${pkgs.gawk}/bin/awk '{print $2}'` == "closed" ]] && systemctl suspend
 
             # Set battery limit to 80% when plugged in
-            [[ `cat /sys/class/power_supply/BAT0/status` == "Charging" ]] && echo 81 > /sys/class/power_supply/BAT0/charge_control_end_threshold
+            [[ `cat /sys/class/power_supply/BAT0/status` == "Charging" ]] && echo ${builtins.toString (batteryTarget + 1)} > /sys/class/power_supply/BAT?/charge_control_end_threshold
         '';
     };
 }
