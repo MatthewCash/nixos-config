97b78eb782b8de92a510d0a16842867a4b1a4aec
Matthew_Cash <matthew@matthew-cash.com>
2022-09-20 13:41:37 -0700

    battery: set limit after driver load

--- config/hardware/battery.nix
+++ config/hardware/battery.nix
@@ -1,10 +1,11 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
+let target = 80; in
 {
     # Limit Battery Charge to 80%
-    systemd.tmpfiles.rules = [
-        "w /sys/class/power_supply/BAT0/charge_control_end_threshold - - - - 81"
-    ];
+    services.udev.extraRules = ''
+        ACTION=="add", KERNEL=="asus-nb-wmi", RUN+="${pkgs.bash}/bin/bash -c 'echo ${builtins.toString (target + 1)} > /sys/class/power_supply/BAT?/charge_control_end_threshold'"
+    '';
 
     powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
