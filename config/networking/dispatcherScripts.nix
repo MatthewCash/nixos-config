f738d0050b6f043e274c578186b99b57f8ad4d52
Matthew_Cash <matthew@matthew-cash.com>
2022-10-15 14:34:01 -0700

    dispatcherScripts: clean up vpn script

--- config/networking/dispatcherScripts.nix
+++ config/networking/dispatcherScripts.nix
@@ -10,24 +10,22 @@
 
     networking.networkmanager.dispatcherScripts = [
         {
+            type = "pre-up";
             source = pkgs.writeText "upHook" ''
                 home_net_id="NETZ_5"
                 current_net_id="$CONNECTION_ID"
+                
+                # Remove previous DNAT rules
+                ${pkgs.nftables}/bin/nft flush chain ip nat zero
+                
+                # Remove VPN routing rules
+                ${pkgs.iproute2}/bin/ip route flush table wg_vpn
 
                 if [[ "$current_net_id" = "$home_net_id" ]]; then
-                    # Remove previous DNAT rules
-                    ${pkgs.nftables}/bin/nft flush chain ip nat zero
-
                     # Add DNAT rules pointing to LAN
                     ${pkgs.nftables}/bin/nft add rule ip nat zero ip daddr 172.30.0.2 counter dnat to 192.168.1.211
                     ${pkgs.nftables}/bin/nft add rule ip nat zero ip daddr 172.30.0.5 counter dnat to 192.168.1.203
-
-                    # Remove VPN routing rules
-                    ${pkgs.iproute2}/bin/ip route flush table wg_vpn
                 else
-                    # Remove previous DNAT rules
-                    ${pkgs.nftables}/bin/nft flush chain ip nat zero
-
                     # Add DNAT rules pointing to VPN
                     ${pkgs.nftables}/bin/nft add rule ip nat zero ip daddr 172.30.0.2 counter dnat to 10.0.0.2
                     ${pkgs.nftables}/bin/nft add rule ip nat zero ip daddr 172.30.0.5 counter dnat to 10.0.0.5
