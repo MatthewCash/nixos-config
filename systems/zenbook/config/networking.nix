d02413cb2e2038b8c8fb4872e66bafc22faaba4b
Matthew_Cash <matthew@matthew-cash.com>
2022-10-20 17:35:59 -0700

    zenbook: use SECLEVEL=0 for wpa_supplicant

--- systems/zenbook/config/networking.nix
+++ systems/zenbook/config/networking.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
     networking.networkmanager = {
@@ -14,4 +14,16 @@
     environment.persistence."/nix/persist".directories = [
         "/etc/NetworkManager/system-connections"
     ];
+
+    systemd.services.wpa_supplicant.environment.OPENSSL_CONF = pkgs.writeText "openssl.cnf" ''
+        openssl_conf = openssl_init
+        [openssl_init]
+        ssl_conf = ssl_sect
+        [ssl_sect]
+        system_default = system_default_sect
+        [system_default_sect]
+        Options = UnsafeLegacyRenegotiation
+        [system_default_sect]
+        CipherString = Default:@SECLEVEL=0
+   '';
 }
