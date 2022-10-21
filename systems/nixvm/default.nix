d8bee4ddbbdf2467cc735f297a42c742d30408e7
Matthew_Cash <matthew@matthew-cash.com>
2022-10-21 14:17:33 -0700

    nixvm: linux kernel 6.0

--- systems/nixvm/default.nix
+++ systems/nixvm/default.nix
@@ -3,7 +3,7 @@
 rec {
     hostname = "nixvm";
     system = "x86_64-linux";
-    kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_5_19;
+    kernelPackages = inputs.nixpkgs.legacyPackages.${system}.linuxPackages_6_0;
 
     nixosConfig = builtins.toString ./config;
     homeConfig = builtins.toString ./home;
