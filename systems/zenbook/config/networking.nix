fae0e6d3a36c3cfc831995467fa35779e2ee6e03
Matthew_Cash <matthew@matthew-cash.com>
2022-10-24 12:22:11 -0700

    zenbook: remove hosts

--- systems/zenbook/config/networking.nix
+++ systems/zenbook/config/networking.nix
@@ -6,11 +6,6 @@
         appendNameservers = [ "1.1.1.1" ];
     };
 
-    networking.hosts = {
-        "172.30.0.2" = [ "epsilon.zero" ];
-        "172.30.0.5" = [ "omicron.zero" ];
-    };
-
     environment.persistence."/nix/persist".directories = [
         "/etc/NetworkManager/system-connections"
     ];
