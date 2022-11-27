a6f3c7de3265b583742cffbc0acaa52e001ddd2d
Matthew_Cash <matthew@matthew-cash.com>
2022-11-27 14:43:18 -0800

    nixvm: add editor-config

--- systems/nixvm/home/matthew/default.nix
+++ systems/nixvm/home/matthew/default.nix
@@ -17,4 +17,5 @@ builtins.map (path: ../../../../home/${homeName}/${path}) [
     "applications/direnv.nix"
     "applications/helix"
     "applications/gradle.nix"
+    "applications/editor-config.nix"
 ]
