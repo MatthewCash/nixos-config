56dd5744addb770d41a143907a1895e359dbf6e7
Matthew_Cash <matthew@matthew-cash.com>
2022-11-30 12:18:42 -0800

    pam-mount: add config module

---
+++ config/pam-mount.nix
@@ -0,0 +1,35 @@
+{ pkgsStable, pkgsUnstable, stableLib, config, ... }:
+
+{
+    security.pam.mount = {
+        enable = true;
+        createMountPoints = true;
+    };
+
+    # Disable home-manager on boot
+    # TODO: enable for gdm!
+    systemd.services = stableLib.attrsets.mapAttrs' (name: value: stableLib.attrsets.nameValuePair "home-manager-${name}" { wantedBy = stableLib.mkForce []; }) config.home-manager.users;
+
+    environment.etc."pam.d/login".text = let
+        systemctl = "${pkgsStable.systemd}/bin/systemctl";
+        checkScript = pkgsStable.writeShellScript "check_for_mount.sh" ''
+            mountpoint -q "/mnt/storage/$PAM_USER"
+        '';
+        hmScript = pkgsStable.writeShellScript "run_hm.sh" ''
+            if [[ -n $(${systemctl} list-unit-files | grep "^home-manager-$PAM_USER") ]]; then
+                ${systemctl} restart home-manager-$PAM_USER
+            fi
+        '';
+
+        # Verify that mount does not already exist
+        pamCheckLine = "session [success=2 default=ignore] ${pkgsStable.linux-pam}/lib/security/pam_exec.so ${checkScript} quiet";
+        pamMountLine = "session optional ${pkgsUnstable.pam_mount}/lib/security/pam_mount.so disable_interactive";
+        # Run home-manager activation for user
+        pamExecLine = "session optional ${pkgsStable.linux-pam}/lib/security/pam_exec.so ${hmScript}";
+
+        pamLoginText = builtins.replaceStrings
+            [ pamMountLine ]
+            [ "${pamCheckLine}\n${pamMountLine}\n${pamExecLine}" ]
+            config.security.pam.services.login.text;
+    in stableLib.mkForce pamLoginText;
+}
