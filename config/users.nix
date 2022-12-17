561d6302e184489990b5dc73259047dd96ccebe3
Matthew_Cash <matthew@matthew-cash.com>
2022-12-16 16:14:15 -0800

    users: create config with function

--- config/users.nix
+++ config/users.nix
@@ -1,18 +1,31 @@
-{ pkgsUnstable, ... }:
+{ pkgsUnstable, stableLib, pamMountUsers, ... }:
+
+let
+    createDisplayName = name: stableLib.strings.toUpper (builtins.substring 0 1 name) +
+        builtins.substring 1 (builtins.stringLength name) name;
+
+    buildUserConfig = name: {
+        inherit name;
+        description = createDisplayName name;
+        passwordFile = "/nix/persist/pwd/${name}";
+        createHome = true;
+        isNormalUser = true;
+        home = "/home/${name}";
+        shell = pkgsUnstable.zsh;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1cahhYmVTV0ewIug2zzGdeXruxWeJToxHDXbEBLoCB ${name}"
+        ];
+    } // stableLib.attrsets.optionalAttrs (builtins.elem name pamMountUsers)  {
+        pamMount = {
+            path = "/dev/main/crypt-home-${name}";
+            mountpoint = "/mnt/storage/${name}";
+            fstype = "crypt";
+        };
+    };
+in
 
 {
     users.mutableUsers = false;
-
-    users.extraUsers.matthew = {
-        description = "Matthew";
-        passwordFile = "/nix/persist/pwd/matthew";
-        createHome = true;
-        isNormalUser = true;
-        home = "/home/matthew";
-        shell = pkgsUnstable.zsh;
-        extraGroups = [ "wheel" "libvirtd" ];
-        openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1cahhYmVTV0ewIug2zzGdeXruxWeJToxHDXbEBLoCB matthew"
-        ];
-    };
+    users.extraUsers.matthew = buildUserConfig "matthew";
 }
