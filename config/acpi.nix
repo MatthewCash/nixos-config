8b1dd9a9f78978b0246677b9746ee3533a51a734
Matthew_Cash <matthew@matthew-cash.com>
2022-12-27 22:29:32 -0800

    acpi: clean up

--- config/acpi.nix
+++ config/acpi.nix
@@ -1,34 +1,34 @@
 { pkgsStable, ... }:
 
-let batteryTarget = 80; in
+let
+    batteryTarget = 80;
+
+    isLidClosed = "[[ \"$(cat /proc/acpi/button/lid/LID/state)\" == \"state:      closed\" ]]";
+    isPowerConnected = "[[ \"$(cat /sys/class/power_supply/AC?/online)\" -eq 0 ]]";
+    isPowerDisconnected = "[[ \"$(cat /sys/class/power_supply/AC?/online)\" -eq 0 ]]";
+    sleep = "${pkgsStable.coreutils}/bin/sleep";
+in
 {
     services.acpid = {
         enable = true;
 
         lidEventCommands = ''
             # If lid is closed...
-            if [[ "$(cat /proc/acpi/button/lid/LID/state)" == "state:      closed" ]]; then
-                # Lock screen
-                ${pkgsStable.systemd}/bin/loginctl lock-sessions
-
+            if ${isLidClosed}; then
                 # Suspend if discharging and if lid is still closed after 15 seconds
-                [[ "$(cat /sys/class/power_supply/BAT?/status)" == "Discharging" ]] && sleep 15 \
-                    && [[ "$(cat /sys/class/power_supply/BAT?/status)" == "Discharging" ]] \
-                    && [[ "$(cat /proc/acpi/button/lid/LID/state)" == "state:      closed" ]] \
-                    && systemctl suspend
+                ${isPowerDisconnected} && ${sleep} 15 && ${isPowerDisconnected} \
+                    && ${isLidClosed} && ${pkgsStable.systemd}/bin/systemctl suspend
             fi
         '';
 
         acEventCommands = ''
             # If the lid is closed and charger is unplugged, suspend after 15 seconds
-            sleep 1 && [[ `cat /sys/class/power_supply/BAT0/status` == "Discharging" ]] \
-                && [[ "$(cat /proc/acpi/button/lid/LID/state)" == "state:      closed" ]] \
-                && sleep 5 && [[ `cat /sys/class/power_supply/BAT0/status` == "Discharging" ]] \
-                && [[ "$(cat /proc/acpi/button/lid/LID/state)" == "state:      closed" ]] \
-                && systemctl suspend
+            ${sleep} 1 && ${isPowerDisconnected} \
+                && ${isLidClosed} && ${sleep} 5 && ${isPowerDisconnected} \
+                && ${isLidClosed} && ${pkgsStable.systemd}/bin/systemctl suspend
 
             # Set battery limit to 80% when plugged in
-            [[ `cat /sys/class/power_supply/BAT0/status` == "Charging" ]] \
+            ${isPowerConnected} \
                 && echo ${builtins.toString (batteryTarget + 1)} > /sys/class/power_supply/BAT?/charge_control_end_threshold
         '';
     };
