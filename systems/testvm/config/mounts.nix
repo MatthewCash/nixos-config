4775c0d7f6657301199d3f7a1f9209c09a17a63b
Matthew_Cash <matthew@matthew-cash.com>
2022-12-28 16:48:47 -0800

    testvm: add lvm and swap

--- systems/testvm/config/mounts.nix
+++ systems/testvm/config/mounts.nix
@@ -4,6 +4,16 @@
     boot.initrd.availableKernelModules = [ "cryptd" ];
 
     boot.initrd.luks.devices = {
-        crypt-nix.device = "/dev/disk/by-label/crypt-nix";
+        lvm = {
+            device = "/dev/disk/by-label/crypt-main";
+            bypassWorkqueues = true;
+        };
     };
+
+    swapDevices = [
+        {
+            device = "/dev/main/swap";
+            randomEncryption.enable = true;
+        }
+    ];
 }
