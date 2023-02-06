3f383416aba5cf7663cd82737b10bce39bce9799
Matthew_Cash <matthew@matthew-cash.com>
2023-02-06 11:36:20 -0800

    webcord: add application

---
+++ home/matthew/applications/webcord.nix
@@ -0,0 +1,98 @@
+{ pkgsStable, pkgsUnstable, persistenceHomePath, name, ... }:
+
+let
+    config = {
+        settings = {
+            general = {
+                menuBar = {
+                    hide = true;
+                };
+                tray = {
+                    disable = false;
+                };
+                taskbar= {
+                    flash = true;
+                };
+                window= {
+                    transparent = true;
+                    hideOnClose = true;
+                };
+            };
+            privacy = {
+                blockApi = {
+                    science = true;
+                    typingIndicator = true;
+                    fingerprinting = true;
+                };
+                permissions= {
+                    video = true;
+                    audio = true;
+                    fullscreen = true;
+                    notifications = true;
+                    display-capture = true;
+                    background-sync = false;
+                };
+            };
+            advanced= {
+                csp = {
+                    enabled = true;
+            };
+            cspThirdParty = {
+                spotify = true;
+                gif = true;
+                hcaptcha = true;
+                youtube = true;
+                twitter = true;
+                twitch = true;
+                streamable = true;
+                vimeo = true;
+                soundcloud = true;
+                paypal = true;
+                audius = true;
+                algolia = true;
+                reddit = true;
+                googleStorageApi = true;
+            };
+            currentInstance = {
+                radio = 0;
+            };
+            devel = {
+                enabled = false;
+            };
+            redirection = {
+                warn = true;
+            };
+            optimize = {
+                gpu = false;
+            };
+            webApi = {
+                webGl = true;
+            };
+            unix = {
+                autoscroll = true;
+            };
+        };
+        update = {
+            notification = {
+                version = "";
+                till = "";
+            };
+        };
+        screenShareStore = {
+            audio = false;
+        };
+    };
+};
+
+    configFile = pkgsStable.writeText "config.json" (builtins.toJSON config);
+in
+
+{
+    home.persistence."${persistenceHomePath}/${name}".directories = [
+        ".config/WebCord"
+    ];
+
+    home.file.".config/WebCord/config.json".source = configFile;
+
+    home.packages = with pkgsUnstable; [ webcord ];
+}
