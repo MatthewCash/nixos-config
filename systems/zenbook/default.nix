e1f560f64d83866a13c2b8a08fcddfbb2f0a1b22
Matthew_Cash <matthew@matthew-cash.com>
2023-02-24 11:39:09 -0800

    zenbook: add accentColor

--- systems/zenbook/default.nix
+++ systems/zenbook/default.nix
@@ -11,6 +11,14 @@ rec {
     pamMountUsers = [ "matthew" ];
     unfreePkgs = [ "steam" "steam-original" ];
     batteryChargeLimit = 100;
+    accentColor = {
+        h = 300;
+        s = 60;
+        l = 70;
+        r = 224;
+        g = 133;
+        b = 224;
+    };
 
     nixosConfig = builtins.toString ./config;
     homeConfig = builtins.toString ./home;
