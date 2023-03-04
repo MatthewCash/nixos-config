9a6a75d04e405e8c1073259221ccd41f4184d273
Matthew_Cash <matthew@matthew-cash.com>
2023-03-03 17:41:25 -0800

    nixvm: set accentColor

--- systems/nixvm/default.nix
+++ systems/nixvm/default.nix
@@ -5,6 +5,14 @@ rec {
     system = "x86_64-linux";
     kernelPackages = nixpkgsUnstable.legacyPackages.${system}.linuxPackages_6_1;
     systemNixpkgs = nixpkgsUnstable;
+    accentColor = {
+        h = 300;
+        s = 60;
+        l = 70;
+        r = 224;
+        g = 133;
+        b = 224;
+    };
 
     nixosConfig = builtins.toString ./config;
     homeConfig = builtins.toString ./home;
