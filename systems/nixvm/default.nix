bfc23eef86c549d18cbc31aafbcbfc5c5ec89f77
Matthew_Cash <matthew@matthew-cash.com>
2023-03-05 18:41:56 -0800

    nixvm: linux kernel 6.2

--- systems/nixvm/default.nix
+++ systems/nixvm/default.nix
@@ -3,7 +3,7 @@
 rec {
     hostname = "nixvm";
     system = "x86_64-linux";
-    kernelPackages = nixpkgsUnstable.legacyPackages.${system}.linuxPackages_6_1;
+    kernelPackages = nixpkgsUnstable.legacyPackages.${system}.linuxPackages_6_2;
     systemNixpkgs = nixpkgsUnstable;
     accentColor = {
         h = 300;
