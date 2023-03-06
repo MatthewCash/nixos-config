7f5f4293f9bda48011ba7a84982b7eb7b750b576
Matthew_Cash <matthew@matthew-cash.com>
2023-03-05 18:40:51 -0800

    testvm: add accentColor

--- systems/testvm/default.nix
+++ systems/testvm/default.nix
@@ -7,6 +7,14 @@ rec {
     systemNixpkgs = nixpkgsUnstable;
     pamMountUsers = [ "matthew" ];
     ssd = false;
+    accentColor = {
+        h = 300;
+        s = 60;
+        l = 70;
+        r = 224;
+        g = 133;
+        b = 224;
+    };
 
     nixosConfig = builtins.toString ./config;
     homeConfig = builtins.toString ./home;
