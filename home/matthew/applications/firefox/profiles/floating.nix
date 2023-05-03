a04578c5f3405dc516e1a98c8fc6e10b81bce897
Matthew_Cash <matthew@matthew-cash.com>
2023-05-02 20:19:05 -0700

    firefox: add layout profile

---
+++ home/matthew/applications/firefox/profiles/floating.nix
@@ -0,0 +1,23 @@
+{ inputs, accentColor, ... }:
+
+{
+    userChrome = /* css */ ''
+        @import "${inputs.firefox-mods}/css/chrome/colors.css";
+
+        :root {
+            --system-hue: ${builtins.toString accentColor.h};
+            --system-saturation: ${builtins.toString accentColor.s}%;
+            --system-lightness: ${builtins.toString accentColor.l}%;
+        }
+    '';
+    userContent = /* css */ ''
+        @import "${inputs.firefox-mods}/userContent.css";
+
+        :root {
+            --system-hue: ${builtins.toString accentColor.h};
+            --system-saturation: ${builtins.toString accentColor.s}%;
+            --system-lightness: ${builtins.toString accentColor.l}%;
+        }
+    '';
+    settings = import ../settings.nix;
+}
