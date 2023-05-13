a43d6bb83db4cf5a9965e7353b86ab67e7418083
Matthew_Cash <matthew@matthew-cash.com>
2023-05-12 19:30:10 -0700

    gnome: get wallpaper path using username

--- home/gnome/config.nix
+++ home/gnome/config.nix
@@ -76,13 +76,13 @@ in
             disable-extension-version-validation = true;
         };
 
+        # Meant to be used with https://github.com/MatthewCash/wallpaper-changer
         "org/gnome/desktop/screensaver" = {
-            picture-uri = "file:///home/matthew/.local/share/backgrounds/current_wallpaper.png";
+            picture-uri = "file:///home/${name}/.local/share/backgrounds/current_wallpaper.png";
         };
-
         "org/gnome/desktop/background" = {
-            picture-uri = "file:///home/matthew/.local/share/backgrounds/current_wallpaper.png";
-            picture-uri-dark = "file:///home/matthew/.local/share/backgrounds/current_wallpaper.png";
+            picture-uri = "file:///home/${name}/.local/share/backgrounds/current_wallpaper.png";
+            picture-uri-dark = "file:///home/${name}/.local/share/backgrounds/current_wallpaper.png";
         };
 
         "org/gnome/desktop/session" = {
