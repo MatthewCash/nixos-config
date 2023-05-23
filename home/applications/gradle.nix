92be4a029e1d3d5faecf2302cde6606da7e12f87
Matthew_Cash <matthew@matthew-cash.com>
2023-05-22 21:00:35 -0700

    gradle: move GRADLE_HOME to XDG config home

--- home/applications/gradle.nix
+++ home/applications/gradle.nix
@@ -1,10 +1,12 @@
-{ pkgsUnstable, stableLib, useImpermanence, persistenceHomePath, name, ... }:
+{ pkgsUnstable, stableLib, useImpermanence, persistenceHomePath, name, config, ... }:
 
 {
     home.persistence."${persistenceHomePath}/${name}".directories = stableLib.mkIf useImpermanence [
-        ".gradle/cache"
-        ".gradle/wrapper"
+        ".config/gradle/cache"
+        ".config/gradle/wrapper"
     ];
+
+    home.sessionVariables.GRADLE_USER_HOME = "${config.xdg.configHome}/gradle";
 
     home.packages = with pkgsUnstable; [ gradle ];
 }
