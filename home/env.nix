6ffe7c22fdc74b5ddf829a35c5a22c977e15c15e
Matthew_Cash <matthew@matthew-cash.com>
2023-06-03 12:57:29 -0700

    env: add variables pointing to nixpkgs

--- home/env.nix
+++ home/env.nix
@@ -1,8 +1,13 @@
-{ pkgsUnstable, ... }:
+{ nixpkgsUnstable, nixpkgsStable, pkgsUnstable, ... }:
 
 {
     home.sessionVariables = rec {
         EDITOR = "${pkgsUnstable.helix}/bin/hx";
         VISUAL = EDITOR;
+    } // {
+        # Same variables from config/nix.nix, for non-NixOS systems
+        nixpkgsStable = builtins.toString nixpkgsStable;
+        nixpkgsUnstable = builtins.toString nixpkgsUnstable;
+        nixpkgs = builtins.toString nixpkgsUnstable;
     };
 }
