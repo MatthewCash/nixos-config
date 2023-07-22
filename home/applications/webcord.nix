668d18dfe82de75581f74f2a572378ff2f6689bd
Matthew_Cash <matthew@matthew-cash.com>
2023-07-22 13:20:50 -0700

    webcord: fix config

--- home/applications/webcord.nix
+++ home/applications/webcord.nix
@@ -1,7 +1,7 @@
-{ pkgsStable, pkgsUnstable, stableLib, useImpermanence, persistenceHomePath, name, ... }:
+{ pkgsStable, pkgsUnstable, stableLib, lib, useImpermanence, persistenceHomePath, name, config, ... }:
 
 let
-    config = {
+    webcordConfig = {
         settings = {
             general = {
                 menuBar = {
@@ -10,10 +10,10 @@ let
                 tray = {
                     disable = false;
                 };
-                taskbar= {
+                taskbar = {
                     flash = true;
                 };
-                window= {
+                window = {
                     transparent = true;
                     hideOnClose = false;
                 };
@@ -33,43 +33,44 @@ let
                     background-sync = false;
                 };
             };
-            advanced= {
+            advanced = {
                 csp = {
                     enabled = true;
-            };
-            cspThirdParty = {
-                spotify = true;
-                gif = true;
-                hcaptcha = true;
-                youtube = true;
-                twitter = true;
-                twitch = true;
-                streamable = true;
-                vimeo = true;
-                soundcloud = true;
-                paypal = true;
-                audius = true;
-                algolia = true;
-                reddit = true;
-                googleStorageApi = true;
-            };
-            currentInstance = {
-                radio = 0;
-            };
-            devel = {
-                enabled = false;
-            };
-            redirection = {
-                warn = true;
-            };
-            optimize = {
-                gpu = false;
-            };
-            webApi = {
-                webGl = true;
-            };
-            unix = {
-                autoscroll = true;
+                };
+                cspThirdParty = {
+                    spotify = true;
+                    gif = true;
+                    hcaptcha = true;
+                    youtube = true;
+                    twitter = true;
+                    twitch = true;
+                    streamable = true;
+                    vimeo = true;
+                    soundcloud = true;
+                    paypal = true;
+                    audius = true;
+                    algolia = true;
+                    reddit = true;
+                    googleStorageApi = true;
+                };
+                currentInstance = {
+                    radio = 0;
+                };
+                devel = {
+                    enabled = false;
+                };
+                redirection = {
+                    warn = true;
+                };
+                optimize = {
+                    gpu = true;
+                };
+                webApi = {
+                    webGl = true;
+                };
+                unix = {
+                    autoscroll = true;
+                };
             };
         };
         update = {
@@ -82,9 +83,8 @@ let
             audio = false;
         };
     };
-};
 
-    configFile = pkgsStable.writeText "config.json" (builtins.toJSON config);
+    configFile = pkgsStable.writeText "config.json" (builtins.toJSON webcordConfig);
 in
 
 {
@@ -92,7 +92,10 @@ in
         ".config/WebCord"
     ];
 
-    home.file.".config/WebCord/config.json".source = configFile;
+    home.activation.webcordConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        $DRY_RUN_CMD ${pkgsUnstable.coreutils}/bin/install -m=644 ${configFile} ${config.xdg.configHome}/WebCord/config.json
+    '';
+
 
     home.packages = with pkgsUnstable; [ webcord ];
 }
