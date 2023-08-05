9449dec2ee3e613d419d8f23f8a81723492aa700
Matthew_Cash <matthew@matthew-cash.com>
2023-08-05 12:53:29 -0700

    gnome: use generators.toINI for kvantum config

--- home/gnome/kvantum.nix
+++ home/gnome/kvantum.nix
@@ -3,10 +3,9 @@
 {
     home.sessionVariables.QT_STYLE_OVERRIDE = stableLib.mkForce "kvantum";
 
-    xdg.configFile."Kvantum/kvantum.kvconfig".text = /* ini */ ''
-        [General]
-        theme=KvLibadwaitaDark
-    '';
+    xdg.configFile."Kvantum/kvantum.kvconfig".text = stableLib.generators.toINI {} {
+        General.theme = "KvLibadwaitaDark";
+    };
     xdg.configFile."Kvantum/KvLibadwaita".source = "${inputs.kvlibadwaita}/src/KvLibadwaita";
 
     home.packages = with pkgsUnstable; [ libsForQt5.qtstyleplugin-kvantum ];
