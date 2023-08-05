e3e2de0d4d780edb1b740e9b14b86c286ee84064
Matthew_Cash <matthew@matthew-cash.com>
2023-08-05 12:52:24 -0700

    plasma: add de config

---
+++ config/de/plasma.nix
@@ -0,0 +1,35 @@
+{ pkgsUnstable, ... }:
+
+{
+    services.xserver = {
+        enable = true;
+
+        displayManager = {
+            defaultSession = "plasmawayland";
+            sddm = {
+                enable = true;
+                autoNumlock = true;
+                theme = "Sweet";
+                settings.Theme.ThemeDir = "${pkgsUnstable.sweet-nova}/share/sddm/themes";
+            };
+        };
+        
+        desktopManager.plasma5.enable = true;
+
+        libinput.enable = true;
+
+        excludePackages = with pkgsUnstable; [ xterm ];
+    };
+
+    environment.plasma5.excludePackages = with pkgsUnstable.plasma5Packages; [
+        plasma-browser-integration
+        konsole
+        oxygen
+    ];
+
+    users.extraUsers.sddm.extraGroups = [ "video" ];
+
+    programs.dconf.enable = true;
+
+    services.dbus.packages = with pkgsUnstable; [ dconf ];
+}
