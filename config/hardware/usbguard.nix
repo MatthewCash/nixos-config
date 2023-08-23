42246d41f511b3b8509455600424f8385b6ba437
Matthew_Cash <matthew@matthew-cash.com>
2023-08-22 20:25:26 -0700

    usbguard: allow keyboard if none are already connected

--- config/hardware/usbguard.nix
+++ config/hardware/usbguard.nix
@@ -5,5 +5,8 @@
         enable = true;
         dbus.enable = true;
         IPCAllowedGroups = [ "usbguard" "wheel" ];
+        rules = ''
+             allow with-interface one-of { 03:00:01 03:01:01 } if !allowed-matches(with-interface one-of { 03:00:01 03:01:01 })
+        '';
     };
 }
