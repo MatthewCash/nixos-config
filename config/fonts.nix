3458661d1b56914b6116ee0e5ae9fb5e055e1172
Matthew_Cash <matthew@matthew-cash.com>
2023-09-13 11:18:26 -0700

    fonts: configure default fonts for entire system

--- config/fonts.nix
+++ config/fonts.nix
@@ -1,9 +1,18 @@
 { pkgsStable, inputs, system, ... }:
 
 {
-    fonts.packages = with pkgsStable; [
-        (nerdfonts.override { fonts = [ "CascadiaCode" ]; })
-        ipafont
-        inputs.aurebesh-fonts.defaultPackage.${system}
-    ];
+    fonts = {
+        packages = with pkgsStable; [
+            (nerdfonts.override { fonts = [ "CascadiaCode" ]; })
+            ipafont
+            inputs.aurebesh-fonts.defaultPackage.${system}
+        ];
+
+        fontconfig.defaultFonts = rec {
+            serif = sansSerif;
+            sansSerif = [ "DejaVu Sans" ];
+            monospace = [ "CaskaydiaCove Nerd Font Mono" ];
+            emoji = [ "Noto Color Emoji" ];
+        };
+    };
 }
