4365f5daff2b5ab18a07fcf6db49d6eb4aecd12e
Matthew_Cash <matthew@matthew-cash.com>
2023-09-27 12:11:12 -0700

    tray-icons-reloaded extension: change icon size to 16px

--- home/gnome/extensions/tray-icons-reloaded.nix
+++ home/gnome/extensions/tray-icons-reloaded.nix
@@ -3,5 +3,8 @@
 {
     home.packages = with pkgsUnstable.gnomeExtensions; [ tray-icons-reloaded ];
 
-    dconf.settings."org/gnome/shell".enabled-extensions = [ "trayIconsReloaded@selfmade.pl" ];
+    dconf.settings = {
+        "org/gnome/shell".enabled-extensions = [ "trayIconsReloaded@selfmade.pl" ];
+        "org/gnome/shell/extensions/trayIconsReloaded".icon-size = 16;
+    };
 }
