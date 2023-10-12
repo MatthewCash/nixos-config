db877f3f5f5b58a7cda38ea5354d6a30bea8bf65
Matthew_Cash <matthew@matthew-cash.com>
2023-10-12 15:01:41 -0700

    tpm-fido: add link to hidraw node in XDG_RUNTIME_DIR

--- home/services/tpm-fido.nix
+++ home/services/tpm-fido.nix
@@ -1,8 +1,21 @@
-{ pkgsUnstable, ... }:
+{ pkgsStable, pkgsUnstable, ... }:
 
 {
     services.tpm-fido = {
         enable = true;
         extraPackages = with pkgsUnstable; [ pinentry-gnome ];
+    };
+
+    systemd.user.services.tpm-fido-node = {
+        Unit.Description = "Create link to tpm-fido hidraw node in XDG_RUNTIME_DIR";
+
+        Service = {
+            Type = "oneshot";
+            ExecStart = pkgsStable.writeShellScript "tpm-fido-node" /* bash */ ''
+                hidraw_node="$(${pkgsStable.coreutils}/bin/basename $(ls /sys/bus/hid/devices/0003:15D9:0A37.0003/hidraw/ | ${pkgsStable.gnused}/bin/sed -n 1p))"
+                ${pkgsStable.coreutils}/bin/ln -s "/dev/$hidraw_node" $XDG_RUNTIME_DIR/tpm-fido-hidrawnode
+            '';
+        };
+        Install.WantedBy = [ "default.target" ];
     };
 }
