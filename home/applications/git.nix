8a1cffb31d79addeaf6b7489c37534ca9a6eb561
Matthew_Cash <matthew@matthew-cash.com>
2023-10-15 14:45:08 -0700

    home/git: auto setup remote

--- home/applications/git.nix
+++ home/applications/git.nix
@@ -15,6 +15,7 @@
         extraConfig = {
             init.defaultBranch = "main";
             core.whitespace = "trailing-space,space-before-tab";
+            push.autoSetupRemote = true;
         };
 	};
 }
