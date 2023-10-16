25705155173d84719074ea6b39c4524771a076c2
Matthew_Cash <matthew@matthew-cash.com>
2023-10-16 14:59:46 -0700

    home/nix: fix undefined package

--- home/applications/nix.nix
+++ home/applications/nix.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ stableLib, pkgsUnstable, ... }:
 
 {
     nix = {
         enable = true;
+
+        package = stableLib.mkForce pkgsUnstable.nix;
+
         settings = {
             experimental-features = [ "nix-command" "flakes" ];
             use-xdg-base-directories = true;
