f6104ba31140fd875d6884d35acae37ff80a09f8
Matthew_Cash <matthew@matthew-cash.com>
2023-10-26 14:49:16 -0700

    home/vscode/client: cleanup config

--- home/applications/vscode/client.nix
+++ home/applications/vscode/client.nix
@@ -1,51 +1,12 @@
-args @ { pkgsStable, pkgsUnstable, inputs, system, ... }:
+args @ { pkgsUnstable, inputs, system, ... }:
 
 let
     vscodium = (pkgsUnstable.vscodium.overrideAttrs (oldAttrs: {
-         desktopItem = pkgsStable.makeDesktopItem {
-            name = "codium";
-            exec = "codium %F";
+         desktopItem = oldAttrs.desktopItem.override {
             icon = "vscode";
-            startupWMClass = "VSCodium";
-            desktopName = "Visual Studio Codium";
-            comment = "Code Editing. Redefined.";
-            genericName = "Text Editor";
-            startupNotify = true;
-            categories = [ "Utility" "TextEditor" "Development" "IDE" ];
-            mimeTypes = [ "text/plain" "inode/directory" ];
         };
     })).override {
         commandLineArgs = "--touch-events";
-    };
-
-    vscodium-fhs = pkgsStable.buildFHSUserEnvBubblewrap {
-        name = "codium";
-
-        targetPkgs = pkgs: (with pkgs; [
-            glibc
-            curl
-            icu
-            libunwind
-            libuuid
-            openssl
-            zlib
-            krb5
-         ]);
-
-        extraInstallCommands = ''
-           ln -s "${vscodium}/share" "$out/"
-        '';
-
-        runScript = "${vscodium}/bin/codium";
-
-        dieWithParent = false;
-
-        meta = {
-            description = ''
-                Wrapped variant of VSCodium which launches in a FHS compatible envrionment.
-                Should allow for easy usage of extensions without nix-specific modifications.
-            '';
-        };
     };
 
     common = import ./common.nix args;
