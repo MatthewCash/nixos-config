19a3c769310f95976e199bfba919c67e21e91c32
Matthew_Cash <matthew@matthew-cash.com>
2023-11-28 17:15:58 -0800

    home/gnome/window-gestures: add extension

---
+++ home/gnome/extensions/window-gestures.nix
@@ -0,0 +1,12 @@
+{ pkgsUnstable, ... }:
+
+{
+    home.packages = with pkgsUnstable.gnomeExtensions; [ window-gestures ];
+
+    dconf.settings."org/gnome/shell".enabled-extensions = [ "windowgestures@extension.amarullz.com" ];
+
+    dconf.settings."org/gnome/shell/extensions/windowgestures" = {
+        taphold-move = false;
+        swipe4-down = 1;
+    };
+}
