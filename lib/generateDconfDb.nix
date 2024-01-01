80812e18307a305cce153294e7348c8724f3a810
Matthew_Cash <matthew@matthew-cash.com>
2024-01-01 14:13:20 -0800

    customLib: add generateDconfDb function

---
+++ lib/generateDconfDb.nix
@@ -0,0 +1,20 @@
+# From https://github.com/nix-community/home-manager/blob/master/modules/misc/dconf.nix
+{ pkgs, lib, inputs }:
+
+let
+    inherit (inputs.home-manager.lib.hm) gvariant;
+    toDconfIni = lib.generators.toINI {
+        mkKeyValue = key: value: "${key}=${toString (gvariant.mkValue value)}";
+    };
+    generateDconfDb = settings: let
+        iniFile = pkgs.writeText "dconf.ini" (toDconfIni settings);
+    in pkgs.runCommand "user" { } /* bash */ ''
+        mkdir key_files
+        ln -s ${iniFile} key_files/
+        ${pkgs.dconf}/bin/dconf compile $out key_files
+    '';
+in
+
+{
+    inherit generateDconfDb;
+}
