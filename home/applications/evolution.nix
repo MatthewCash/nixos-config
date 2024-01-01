6456196274a288cda9a8f691a42cb4d9d0ba4ff1
Matthew_Cash <matthew@matthew-cash.com>
2024-01-01 14:14:27 -0800

    home/evolution: add dconf db to sandbox

--- home/applications/evolution.nix
+++ home/applications/evolution.nix
@@ -1,7 +1,59 @@
-{ stableLib, pkgsStable, pkgsUnstable, inputs, useImpermanence, config, systemConfig, persistenceHomePath, name, ... }:
+{ stableLib, pkgsStable, pkgsUnstable, inputs, useImpermanence, config, systemConfig, persistenceHomePath, name, customLib, ... }:
 
 let
     systemConfigOptionals = stableLib.optionals (systemConfig != null);
+
+    dconfSettings = {
+        "org/gnome/evolution/mail" = {
+            layout = 1;
+            show-to-do-bar = false;
+            show-to-do-bar-sub = false;
+            mark-seen-timeout = 0;
+            prompt-on-mark-all-read = false;
+        };
+        "org/gnome/evolution/mail/browser-window" = {
+            height = 400;
+            width = 600;
+            maximized = false;
+        };
+        "org/gnome/evolution/mail/composer-window" = {
+            height = 800;
+            width = 1150;
+            show-to-do-bar = false;
+            show-to-do-bar-sub = false;
+            maximized = false;
+        };
+        "org/gnome/evolution/shell" = {
+            attachment-view = 0;
+            autoar-filter = "none";
+            autoar-format = "zip";
+            buttons-visible = true;
+            buttons-visible-sub = true;
+            default-component-id = "mail";
+            folder-bar-width = 263;
+            folder-bar-width-sub = 259;
+            menubar-visible = false;
+            menubar-visible-sub = false;
+            sidebar-visible = true;
+            sidebar-visible-sub = true;
+            statusbar-visible = false;
+            statusbar-visible-sub = true;
+            toolbar-visible = false;
+            toolbar-visible-sub = true;
+        };
+        "org/gnome/evolution/shell/window" = {
+            height = 1080;
+            maximized = false;
+            width = 1920;
+            x = 0;
+            y = 0;
+        };
+        "org/gnome/evolution/plugin/prefer-plain" = {
+            mode = "prefer_source"; # Prefer plain-text messages
+        };
+    };
+
+    dconfDb = customLib.generateDconfDb dconfSettings;
 
     wrappedEvolution = inputs.nixpak.lib.nixpak { lib = stableLib; pkgs = pkgsStable; } {
         config = { sloth, ... }: {
@@ -40,8 +92,9 @@ let
                     [ ("${config.gtk.cursorTheme.package}/share/icons") (sloth.concat' sloth.xdgDataHome "/icons") ]
                     [ ("${config.gtk.theme.package}/share/themes") (sloth.concat' sloth.xdgDataHome "/themes") ]
                     (sloth.concat' sloth.xdgConfigHome "/gtk-3.0")
+                    [ dconfDb (sloth.concat' sloth.xdgConfigHome "/dconf/user") ]
                 ];
-                extraStorePaths = (
+                extraStorePaths = [ dconfDb ] ++ (
                     stableLib.attrsets.mapAttrsToList
                         (n: v: v.source)
                         (stableLib.attrsets.filterAttrs (n: v: stableLib.strings.hasPrefix "${config.xdg.configHome}/gtk-3.0" n) config.home.file)
@@ -67,58 +120,4 @@ in
     ];
 
     home.packages = [ wrappedEvolution.config.env ];
-
-    dconf.settings."org/gnome/evolution/mail" = {
-        layout = 1;
-        show-to-do-bar = false;
-        show-to-do-bar-sub = false;
-        mark-seen-timeout = 0;
-        prompt-on-mark-all-read = false;
-    };
-
-    dconf.settings."org/gnome/evolution/mail/browser-window" = {
-        height = 400;
-        width = 600;
-        maximized = false;
-    };
-
-    dconf.settings."org/gnome/evolution/mail/composer-window" = {
-        height = 800;
-        width = 1150;
-        show-to-do-bar = false;
-        show-to-do-bar-sub = false;
-        maximized = false;
-    };
-
-    dconf.settings."org/gnome/evolution/shell" = {
-        attachment-view = 0;
-        autoar-filter = "none";
-        autoar-format = "zip";
-        buttons-visible = true;
-        buttons-visible-sub = true;
-        default-component-id = "mail";
-        folder-bar-width = 263;
-        folder-bar-width-sub = 259;
-        menubar-visible = false;
-        menubar-visible-sub = false;
-        sidebar-visible = true;
-        sidebar-visible-sub = true;
-        statusbar-visible = false;
-        statusbar-visible-sub = true;
-        toolbar-visible = false;
-        toolbar-visible-sub = true;
-    };
-
-    dconf.settings."org/gnome/evolution/shell/window" = {
-        height = 1080;
-        maximized = false;
-        width = 1920;
-        x = 0;
-        y = 0;
-    };
-
-    # Prefer plain text messages
-    dconf.settings."org/gnome/evolution/plugin/prefer-plain" = {
-        mode = "prefer_source";
-    };
 }
