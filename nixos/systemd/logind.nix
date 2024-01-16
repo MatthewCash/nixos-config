56b4c1faf40a679085c952f96de14825f9e24064
Matthew_Cash <matthew@matthew-cash.com>
2024-01-16 14:47:24 -0800

    nixos/logind: explicitly set runtime dir size and max inodes

--- nixos/systemd/logind.nix
+++ nixos/systemd/logind.nix
@@ -4,5 +4,7 @@
      services.logind.extraConfig = /* ini */ ''
         IdleActionSec=30s
         HoldoffTimeoutSec=3s
+        RuntimeDirectorySize=2G
+        RuntimeDirectoryInodesMax=2M
 	 '';
 }
