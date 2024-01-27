c3672c837ed200ae8ce297193884eff6d34196a7
Matthew_Cash <matthew@matthew-cash.com>
2024-01-27 12:21:55 -0800

    home/rust: set CARGO_HOME to be in XDG_DATA_HOME

--- home/dev/rust.nix
+++ home/dev/rust.nix
@@ -1,4 +1,4 @@
-{ pkgsUnstable, ... }:
+{ pkgsUnstable, config, ... }:
 
 {
     home.packages = with pkgsUnstable; [
@@ -8,5 +8,6 @@
 
     home.sessionVariables = {
         RUST_SRC_PATH = "${pkgsUnstable.rust.packages.stable.rustPlatform.rustLibSrc}";
+        CARGO_HOME = "${config.xdg.dataHome}/cargo";
     };
 }
