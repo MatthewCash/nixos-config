b38ef8a730bce2bd7e2285de36a1e53da3b47290
Matthew_Cash <matthew@matthew-cash.com>
2024-02-29 21:46:43 -0800

    home/gnome/blur-my-shell: blur panel only in overview

--- home/gnome/extensions/blur-my-shell.nix
+++ home/gnome/extensions/blur-my-shell.nix
@@ -4,4 +4,8 @@
     home.packages = with pkgsUnstable.gnomeExtensions; [ blur-my-shell ];
 
     dconf.settings."org/gnome/shell".enabled-extensions = [ "blur-my-shell@aunetx" ];
+
+    dconf.settings."org/gnome/shell/extensions/blur-my-shell/panel" = {
+        override-background-dynamically = true;
+    };
 }
