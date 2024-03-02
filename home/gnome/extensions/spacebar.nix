99954dc5d351578298b5f7ce50f79bccc9faaf37
Matthew_Cash <matthew@matthew-cash.com>
2024-03-02 14:54:41 -0800

    home/gnome/extensions: remove unneeded spacebar

--- home/gnome/extensions/spacebar.nix
+++
@@ -1,7 +0,0 @@
-{ pkgsUnstable, ... }:
-
-{
-    home.packages = with pkgsUnstable.gnomeExtensions; [ space-bar ];
-
-    dconf.settings."org/gnome/shell".enabled-extensions = [ "space-bar@luchrioh" ];
-}
