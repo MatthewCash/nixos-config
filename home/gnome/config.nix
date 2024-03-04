f46bd74b87dc0cd37a42d401deddf10230410edc
Matthew_Cash <matthew@matthew-cash.com>
2024-03-04 11:10:02 -0800

    home/gnome: fix webcord desktop item

--- home/gnome/config.nix
+++ home/gnome/config.nix
@@ -56,7 +56,7 @@ in
 
         "org/gnome/shell" = {
             favorite-apps = [
-                "webcord.desktop"
+                "org.discord.webcord.desktop"
                 "org.google.chromium.desktop"
                 "termius-app.desktop"
                 "codium.desktop"
