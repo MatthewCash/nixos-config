5eb9f4be84cc363592e814bd1dbea65cbd2689c3
Matthew_Cash <matthew@matthew-cash.com>
2024-05-20 20:30:30 -0700

    nixos/tailscale: automatically determine fw implementation

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -3,6 +3,10 @@
 {
     services.tailscale.enable = true;
 
+    systemd.services.tailscaled.serviceConfig.Environment = [
+        "TS_DEBUG_FIREWALL_MODE=auto"
+    ];
+
     age.secrets."tailscale-${tailscaleId}" = {
         file = ../secrets/tailscale/${tailscaleId}.age;
         path = "/var/lib/tailscale/tailscaled.state";
