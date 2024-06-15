def1c3f63c82f815d29a5fc0876edc11cbf453fa
Matthew_Cash <matthew@matthew-cash.com>
2024-06-15 13:17:43 -0700

    nixos/tailscale: change max log level to 'notice'

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -3,9 +3,12 @@
 {
     services.tailscale.enable = true;
 
-    systemd.services.tailscaled.serviceConfig.Environment = [
-        "TS_DEBUG_FIREWALL_MODE=auto"
-    ];
+    systemd.services.tailscaled.serviceConfig = {
+        LogLevelMax = "notice";
+        Environment = [
+            "TS_DEBUG_FIREWALL_MODE=auto"
+        ];
+    };
 
     age.secrets."tailscale-${tailscaleId}" = {
         file = ../secrets/tailscale/${tailscaleId}.age;
