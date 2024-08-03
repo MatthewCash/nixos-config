8bb61f4982f5c8d6bc83c9d3a7467fec099b60e6
Matthew_Cash <matthew@matthew-cash.com>
2024-08-03 13:44:25 -0700

    nixos/sound: disable alsa

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -5,8 +5,6 @@
 
     services.pipewire = {
         enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
     };
