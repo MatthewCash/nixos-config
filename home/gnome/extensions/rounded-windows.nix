5ea1376df1b82e81a857f459d6704975e43ddfdd
Matthew_Cash <matthew@matthew-cash.com>
2024-11-11 22:03:33 -0800

    home/gnome: remove rounded-windows extension

--- home/gnome/extensions/rounded-windows.nix
+++
@@ -1,7 +0,0 @@
-{ pkgsUnstable, ... }:
-
-{
-    home.packages = with pkgsUnstable.gnomeExtensions; [ rounded-window-corners ];
-
-    dconf.settings."org/gnome/shell".enabled-extensions = [ "rounded-window-corners@yilozt" ];
-}
