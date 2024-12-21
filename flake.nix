a1b64658807df1019162c90cdae9ff4337b2ad68
Matthew_Cash <matthew@matthew-cash.com>
2024-12-20 22:55:43 -0800

    Switch back to locally sourced inputs

--- flake.nix
+++ flake.nix
@@ -1,13 +1,117 @@
 {
     description = "NixOS and Home Manager Configuration";
 
-    inputs.nix-inputs.url = "github:MatthewCash/nix-inputs";
+    inputs = {
+        asus-wmi-screenpad = {
+            url = "github:MatthewCash/asus-wmi-screenpad-module";
+            inputs.nixpkgs.follows = "nixpkgsStable";
+        };
 
-    outputs = originalInputs @ { nix-inputs, ... }:
+        aurebesh-fonts = {
+            url = "github:MatthewCash/aurebesh-fonts";
+            inputs.nixpkgs.follows = "nixpkgsStable";
+        };
+
+        codium-theme = {
+            url = "github:MatthewCash/codium-theme";
+            inputs.nixpkgs.follows = "nixpkgsStable";
+        };
+
+        discord-css = {
+            url = "github:MatthewCash/discord-css";
+            flake = false;
+        };
+
+        disko = {
+            url = "github:nix-community/disko";
+            inputs.nixpkgs.follows = "nixpkgsStable";
+        };
+
+        firefox-gnome-theme = {
+            url = "github:rafaelmardojai/firefox-gnome-theme/beta";
+            flake = false;
+        };
+
+        firefox-mods = {
+            url = "github:MatthewCash/firefox-mods";
+            flake = false;
+        };
+
+        flake-utils.url = "github:numtide/flake-utils";
+
+        gnome-accent-colors = {
+            url = "github:demiskp/custom-accent-colors/v6";
+            flake = false;
+        };
+
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgsUnstable";
+        };
+
+        impermanence.url = "github:nix-community/impermanence";
+
+        kvlibadwaita = {
+            url = "github:GabePoel/KvLibadwaita";
+            flake = false;
+        };
+
+        lanzaboote = {
+            url = "github:nix-community/lanzaboote/v0.4.1";
+            inputs.nixpkgs.follows = "nixpkgsStable";
+        };
+
+        mozilla-theme = {
+            url = "github:MatthewCash/mozilla-theme";
+            inputs.nixpkgs.follows = "nixpkgsStable";
+        };
+
+        nixos-generators = {
+            url = "github:nix-community/nixos-generators";
+            inputs.nixpkgs.follows = "nixpkgsUnstable";
+        };
+
+        nixpkgsStable.url = "github:nixos/nixpkgs/nixos-24.11";
+        nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+        nixpak = {
+            url = "github:MatthewCash/nixpak";
+            inputs.nixpkgs.follows = "nixpkgsUnstable";
+        };
+
+        owl-patched = {
+            url = "github:MatthewCash/owl-patched";
+            inputs.nixpkgs.follows = "nixpkgsUnstable";
+        };
+
+        plasma-manager = {
+            url = "github:MatthewCash/plasma-manager";
+            inputs.nixpkgs.follows = "nixpkgsUnstable";
+        };
+
+        ragenix = {
+            url = "github:yaxitech/ragenix";
+            inputs.nixpkgs.follows = "nixpkgsStable";
+        };
+
+        sweet-kde = {
+            url = "github:EliverLara/Sweet-kde";
+            flake = false;
+        };
+
+        tpm-fido = {
+            url = "github:MatthewCash/tpm-fido";
+            inputs.nixpkgs.follows = "nixpkgsStable";
+        };
+
+        zsh-nix-shell = {
+            url = "github:MatthewCash/zsh-nix-shell";
+            flake = false;
+        };
+    };
+
+    outputs = inputs @ { nixpkgsStable, nixpkgsUnstable, flake-utils, ... }:
     let
-        inputs = nix-inputs.outputs.inputs // builtins.removeAttrs originalInputs [ "nix-inputs" ];
-        inherit (inputs) nixpkgsStable nixpkgsUnstable flake-utils;
-
         stableLib = nixpkgsStable.lib;
 
         stateVersion = "24.11";
