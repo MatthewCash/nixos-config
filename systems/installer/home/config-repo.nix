601efd77b3e587dff5d703babc8aa7ad05e242ea
Matthew_Cash <matthew@matthew-cash.com>
2024-12-21 16:04:28 -0800

    installer/home/config-repo: copy instead directory instead of linking into store

--- systems/installer/home/config-repo.nix
+++ systems/installer/home/config-repo.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ lib, ... }:
 
 {
-    home.file."nixos-config".source = ../../..;
+    home.activation.configRepo = lib.hm.dag.entryAfter [ "writeBoundary" ] /* sh */ ''
+        $DRY_RUN_CMD cp -r ${../../..} ~/nixos-config
+        $DRY_RUN_CMD chown -R $USER ~/nixos-config
+        $DRY_RUN_CMD chmod 755 ~/nixos-config
+    '';
 
     programs.zsh.initExtra = /* zsh */ ''
         cd ~/nixos-config
