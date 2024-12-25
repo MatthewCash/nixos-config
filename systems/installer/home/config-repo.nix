9def83f5c633f93f1c84de185adf93690ddb71f5
Matthew_Cash <matthew@matthew-cash.com>
2024-12-24 21:54:05 -0800

    installer/home/config-repo: set repo mode recursively

--- systems/installer/home/config-repo.nix
+++ systems/installer/home/config-repo.nix
@@ -4,7 +4,7 @@
     home.activation.configRepo = lib.hm.dag.entryAfter [ "writeBoundary" ] /* sh */ ''
         $DRY_RUN_CMD cp -r ${../../..} ~/nixos-config
         $DRY_RUN_CMD chown -R $USER ~/nixos-config
-        $DRY_RUN_CMD chmod 755 ~/nixos-config
+        $DRY_RUN_CMD chmod -R 755 ~/nixos-config
     '';
 
     programs.zsh.initExtra = /* zsh */ ''
