37d992196e8aa81778681c3fb471e3ebe7e71e45
Matthew_Cash <matthew@matthew-cash.com>
2025-01-02 18:01:39 -0800

    nixvm/networing: use networkd to manage interfaces

--- systems/nixvm/nixos/networking.nix
+++ systems/nixvm/nixos/networking.nix
@@ -1,25 +1,22 @@
 { ... }:
 
 let
-    systemAddress = "192.168.40.2";
-    prefixLength = 24;
+    systemAddress = "192.168.40.2/24";
     gatewayAddress = "192.168.40.1";
 in
 
 {
     networking = {
-        hosts = {
-            "${gatewayAddress}" = [ "host" "windows" "inferno" ];
-        };
-
+        useDHCP = false;
+        hosts.${gatewayAddress} = [ "host" ];
         nameservers = [ "100.100.100.100" "1.1.1.1" ];
-        resolvconf.extraOptions = [ "use-vc" ];
+    };
 
-        interfaces.eth0.ipv4.addresses = [{
-            inherit prefixLength;
-            address = systemAddress;
-        }];
+    systemd.network.enable = true;
 
-        defaultGateway = gatewayAddress;
+    systemd.network.networks."20-hyperswitch" = {
+        matchConfig.Name = "eth0";
+        address = [ systemAddress ];
+        routes = [ { Gateway = gatewayAddress; } ];
     };
 }
