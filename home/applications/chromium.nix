7717d1f2cbb84f8c3ed3baac3f692370b561c97d
Matthew_Cash <matthew@matthew-cash.com>
2025-01-25 11:45:48 -0800

    home/chromium: add gpu to sandbox

--- home/applications/chromium.nix
+++ home/applications/chromium.nix
@@ -28,6 +28,7 @@ let
             };
             locale.enable = true;
             etc.sslCertificates.enable = true;
+            gpu.enable = true;
             bubblewrap = {
                 bindEntireStore = false;
                 bind.rw = [
