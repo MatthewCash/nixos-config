1cf8f980f8b5007daf1ee19913fe712c85cf3db3
Matthew_Cash <matthew@matthew-cash.com>
2025-03-03 10:10:35 -0800

    home/vscode/client: move config to default profile

--- home/applications/vscode/client.nix
+++ home/applications/vscode/client.nix
@@ -14,26 +14,28 @@ in
     programs.vscode = {
         enable = true;
         package = vscodium;
-        enableUpdateCheck = false;
-        enableExtensionUpdateCheck = false;
-        extensions = with pkgsUnstable.vscode-extensions; [
-            inputs.codium-theme.defaultPackage.${system}
-            tomoki1207.pdf
-        ] ++ common.extensions;
-        userSettings = common.settings // {
-            "workbench.colorTheme" = "Main Theme";
-            "window.titleBarStyle" = "custom";
-            "editor.smoothScrolling" = true;
-            "editor.cursorSmoothCaretAnimation" = "on";
-            "workbench.list.smoothScrolling" = true;
-            "terminal.integrated.smoothScrolling" = true;
-            "editor.renderWhitespace" = "all";
-            "files.trimTrailingWhitespace" = true;
-            "files.insertFinalNewline" = true;
-            "files.trimFinalNewlines" = true;
-            "editor.wordWrap" = "on";
-            "window.commandCenter" = true;
-            "editor.fontFamily" = "'CaskaydiaCove Nerd Font Regular', monospace";
+        profiles.default = {
+            enableUpdateCheck = false;
+            enableExtensionUpdateCheck = false;
+            extensions = with pkgsUnstable.vscode-extensions; [
+                inputs.codium-theme.defaultPackage.${system}
+                tomoki1207.pdf
+            ] ++ common.extensions;
+            userSettings = common.settings // {
+                "workbench.colorTheme" = "Main Theme";
+                "window.titleBarStyle" = "custom";
+                "editor.smoothScrolling" = true;
+                "editor.cursorSmoothCaretAnimation" = "on";
+                "workbench.list.smoothScrolling" = true;
+                "terminal.integrated.smoothScrolling" = true;
+                "editor.renderWhitespace" = "all";
+                "files.trimTrailingWhitespace" = true;
+                "files.insertFinalNewline" = true;
+                "files.trimFinalNewlines" = true;
+                "editor.wordWrap" = "on";
+                "window.commandCenter" = true;
+                "editor.fontFamily" = "'CaskaydiaCove Nerd Font Regular', monospace";
+            };
         };
     };
 }
