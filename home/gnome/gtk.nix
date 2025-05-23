be71a08982add0a3860dcc1e154db122087cbd51
Matthew_Cash <matthew@matthew-cash.com>
2025-05-22 19:29:25 -0700

    home/gnome: remove IDF from shell accent coloring

--- home/gnome/gtk.nix
+++ home/gnome/gtk.nix
@@ -11,18 +11,14 @@ let
         }
     '';
 
-    oldShellCss = pkgsStable.runCommand "gnome-shell-css" { } /* bash */ ''
+    shellCss = pkgsStable.runCommand "gnome-shell-css" { } /* bash */ ''
         ${pkgsUnstable.glib.dev}/bin/gresource \
              extract \
             ${pkgsUnstable.gnome-shell}/share/gnome-shell/gnome-shell-theme.gresource \
             /org/gnome/shell/theme/gnome-shell-dark.css \
+        | sed s/-st-accent-color/${accentColor.hex}/g\
         > $out
     '';
-
-    shellCss = builtins.replaceStrings
-        [ "-st-accent-color" ]
-        [ accentColor.hex ]
-        (builtins.readFile oldShellCss);
 in
 
 {
@@ -52,7 +48,7 @@ in
     };
 
     home.packages = with pkgsUnstable.gnomeExtensions; [ user-themes ];
-    xdg.dataFile."themes/shell-theme/gnome-shell/gnome-shell.css".text = shellCss;
+    xdg.dataFile."themes/shell-theme/gnome-shell/gnome-shell.css".source = shellCss;
     dconf.settings = {
         "org/gnome/shell".enabled-extensions = [ "user-theme@gnome-shell-extensions.gcampax.github.com" ];
         "org/gnome/shell/extensions/user-theme".name = "shell-theme";
