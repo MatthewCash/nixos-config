22f8c5c96e027b7c6038d2deecf39e5495b6dd38
Matthew_Cash <matthew@matthew-cash.com>
2025-06-03 19:23:20 -0700

    nixos/disks: hide boot contents from non-root

--- nixos/fs/disks.nix
+++ nixos/fs/disks.nix
@@ -34,6 +34,7 @@
                     type = "filesystem";
                     format = "vfat";
                     mountpoint = "/boot";
+                    mountOptions = [ "umask=0077" ];
                 };
             };
         };
