094795b0d2859047b5b80db6e8cf75701d77d584
Matthew_Cash <matthew@matthew-cash.com>
2025-08-07 19:19:41 -0700

    nixos/systemd: fix eval

--- nixos/systemd/config.nix
+++ nixos/systemd/config.nix
@@ -2,9 +2,8 @@
 
 {
     # Limit "A stop job is running..." delay
-    systemd.extraConfig = /* ini */ ''
-        DefaultTimeoutStopSec=10s
-    '';
+    systemd.settings.Manager.DefaultTimeoutStopSec = "10s";
+
     systemd.user.extraConfig = /* ini */ ''
         DefaultTimeoutStopSec=10s
     '';
