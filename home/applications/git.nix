0a49d5b434485c0a9713d324725ce7d2fe646587
Matthew_Cash <matthew@matthew-cash.com>
2025-11-13 18:13:37 -0800

    home/git: use new settings api

--- home/applications/git.nix
+++ home/applications/git.nix
@@ -4,22 +4,24 @@
     programs.git = {
         enable = true;
 
-        userName = "Matthew_Cash";
-        userEmail = "matthew@matthew-cash.com";
+        settings = {
+            user = {
+                name = "Matthew_Cash";
+                email = "matthew@matthew-cash.com";
+            };
+
+            init.defaultBranch = "main";
+            core.whitespace = "trailing-space,space-before-tab";
+            push.autoSetupRemote = true;
+        };
 
         signing = {
             signByDefault = true;
             key = "7449B33FA8E4C190";
         };
 
-        extraConfig = {
-            init.defaultBranch = "main";
-            core.whitespace = "trailing-space,space-before-tab";
-            push.autoSetupRemote = true;
-        };
-
-        difftastic.enable = true;
-
         lfs.enable = true;
 	};
+
+	programs.difftastic.enable = true;
 }
