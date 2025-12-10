c1b82d07e3641b51f2c81efdf3ca447da3932a38
Matthew_Cash <matthew@matthew-cash.com>
2025-12-10 12:37:08 -0800

    Bump lanzaboote to v1.0.0

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
         };
 
         lanzaboote = {
-            url = "github:nix-community/lanzaboote/v0.4.3";
+            url = "github:nix-community/lanzaboote/v1.0.0";
             inputs.nixpkgs.follows = "nixpkgsStable";
         };
 
