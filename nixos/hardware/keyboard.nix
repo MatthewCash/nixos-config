6e20976e95c4c64f141b0312422543bbd8b62f9f
Matthew_Cash <matthew@matthew-cash.com>
2025-12-18 12:59:14 -0800

    nixos/keyboard: configure shortcuts globally with kanata

--- nixos/hardware/keyboard.nix
+++ nixos/hardware/keyboard.nix
@@ -5,4 +5,30 @@
         layout = "us";
         variant = "colemak_dh";
     };
+
+    services.kanata = {
+        enable = true;
+        keyboards.default = {
+            extraDefCfg = "process-unmapped-keys yes";
+            config = /* lisp */ ''
+                (defsrc
+                    caps
+                    mfwd
+                    mbck
+                )
+
+                ;; set some aliases for raising/lowering windows
+                (defalias
+                    mf (fork mfwd (multi (release-key lctl) M-pgup) (lctl mmid))
+                    mb (fork mbck (multi (release-key lctl) M-pgdn) (lctl mmid))
+                )
+
+                (deflayer default
+                    esc ;; map caps locks -> escape
+                    @mf
+                    @mb
+                )
+            '';
+        };
+    };
 }
