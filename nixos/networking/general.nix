6e7f2d99f56e6785910ec2cb708e738c852ba45d
Matthew_Cash <matthew@matthew-cash.com>
2025-12-27 11:48:14 -0800

    nixos/networking: try to reduce tcp latency

--- nixos/networking/general.nix
+++ nixos/networking/general.nix
@@ -2,4 +2,10 @@
 
 {
     networking.hostName = hostname;
+
+    boot.kernel.sysctl = {
+        "net.ipv4.tcp_low_latency" = 1;
+        "net.ipv4.tcp_delack_min" = 0;
+        "net.ipv4.tcp_fastopen" = 3;
+    };
 }
