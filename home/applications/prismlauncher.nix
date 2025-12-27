f06bce4032900fe40b3f34f989dd12f623dc5cca
Matthew_Cash <matthew@matthew-cash.com>
2025-12-27 11:47:33 -0800

    home/prismlauncher: sandbox the launcher

--- home/applications/prismlauncher.nix
+++ home/applications/prismlauncher.nix
@@ -1,9 +1,48 @@
-{ pkgsUnstable, stableLib, useImpermanence, persistenceHomePath, name, ... }:
+{ pkgsStable, pkgsUnstable, config, stableLib, useImpermanence, persistenceHomePath, name, inputs, ... }:
+
+let
+    wrappedPrismlauncher = inputs.nixpak.lib.nixpak { lib = stableLib; pkgs = pkgsStable; } {
+        config = { sloth, ... }: {
+            app.package = pkgsUnstable.prismlauncher;
+            dbus.policies = {
+                "org.freedesktop.portal.*" = "talk";
+                "ca.desrt.dconf" = "talk";
+                "org.a11y.Bus" = "talk";
+                "org.freedesktop.Notifications" = "talk";
+            };
+            locale.enable = true;
+            etc.sslCertificates.enable = true;
+            flatpak.session-helper.enable = true;
+            gpu.enable = true;
+            bubblewrap = {
+                bind.rw = [
+                    (sloth.concat' sloth.xdgDataHome "/PrismLauncher")
+                    (sloth.concat' sloth.runtimeDir "/doc") # For the Document portal
+                    [(sloth.concat' sloth.xdgDataHome "/PrismLauncher/tmp") "/tmp"]
+                    (sloth.concat' sloth.xdgStateHome "/nix/profile/share/Kvantum")
+                    (sloth.concat' sloth.xdgStateHome "/nix/profile/lib/qt-6/plugins")
+                    (sloth.concat' sloth.xdgConfigHome "/Kvantum")
+                ];
+                bind.ro = [
+                    "/run/current-system"
+                    "/etc/fonts"
+                    (builtins.toString config.home-files) # Not in extraStorePaths because we do not want it recursively linked
+                ];
+                sockets = {
+                    wayland = true;
+                    x11 = true; # mc instances don't work quite right with wayland yet
+                    pipewire = true;
+                    pulse = true;
+                };
+            };
+        };
+    };
+in
 
 {
     home.persistence."${persistenceHomePath}/${name}".directories = stableLib.mkIf useImpermanence [
         ".local/share/PrismLauncher"
     ];
 
-    home.packages = with pkgsUnstable; [ prismlauncher ];
+    home.packages = [ wrappedPrismlauncher.config.env ];
 }
