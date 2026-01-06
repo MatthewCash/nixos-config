3b2cbbd0a23b47e68064d4b949e0722309ad4ab1
Matthew_Cash <matthew@matthew-cash.com>
2026-01-05 16:30:11 -0800

    nixos: add config for logi g920 wheel

---
+++ nixos/hardware/wheel.nix
@@ -0,0 +1,12 @@
+{ pkgsUnstable, ... }:
+
+{
+    # This is for a Logitech G920
+    services.udev = {
+        packages = with pkgsUnstable; [ oversteer ];
+        extraRules = ''
+            ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c261", RUN+="${pkgsUnstable.usb-modeswitch}/bin/usb_modeswitch -v 046d -p c261 -m 01 -r 01 -C 03 -M '0f00010142'"
+        '';
+    };
+    hardware.new-lg4ff.enable = true;
+}
