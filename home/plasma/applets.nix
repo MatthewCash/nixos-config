38c0b938f37d3f6a8eeabdd606b8accb31847887
Matthew_Cash <matthew@matthew-cash.com>
2026-01-25 09:45:50 -0800

    home/plasma: move applets to per-system config

--- home/plasma/applets.nix
+++ home/plasma/applets.nix
@@ -9,43 +9,5 @@ in
         directories = [ ".local/share/backgrounds" ];
     };
 
-    home.packages = with pkgsUnstable; [ nixos-icons ];
-
     home.sessionVariables.STATIC_WALLPAPER_PATH = wallpaperPath;
-
-    programs.plasma.panels = [
-        {
-            location = "bottom";
-            widgets = [
-                {
-                    name = "org.kde.plasma.kickoff";
-                    config.General.icon = "nix-snowflake-white";
-                }
-                {
-                    name = "org.kde.plasma.taskmanager";
-                    config.General = {
-                        showOnlyCurrentScreen = true;
-                        taskMaxWidth = "Narrow";
-                        launchers = "";
-                    };
-                }
-                "org.kde.plasma.systemmonitor.cpucore"
-                "org.kde.plasma.systemmonitor.memory"
-                "org.kde.plasma.systemtray"
-                {
-                    name = "org.kde.plasma.digitalclock";
-                    config.Appearance = {
-                        showSeconds = "Always";
-                        showWeekNumbers = true;
-                        dateFormat = "isoDate";
-
-                        autoFontAndSize = false;
-                        fontSize = 12;
-                        fontStyleName = "Regular";
-                        fontWeight = 400;
-                    };
-                }
-            ];
-        }
-    ];
 }
