54fcff86b3d91d3b5a8fd7e533d9b249b2ca9b11
Matthew_Cash <matthew@matthew-cash.com>
2026-02-08 08:49:08 -0800

    mango/nixos/disks: add windows mounts

--- systems/mango/nixos/disks.nix
+++ systems/mango/nixos/disks.nix
@@ -1,6 +1,35 @@
-{ ... }:
+{ stableLib, ... }:
+
+let
+    windowsMounts = {
+        windows = "e3fe1ee6-8bce-44d5-bf65-3f93f053dac8";
+        storage = "1c6496e9-412a-4b4d-8da1-49e33a9eaf92";
+        archive = "0f7176cf-2f44-408e-8357-bc9e9fd4a25f";
+        data = "5047919b-bf8a-4fb4-8b7a-406c24abba30";
+    };
+
+    mkWindowsMounts = names: builtins.map (name: {
+        description = "Mount BitLocker volume ${name}";
+        requires = [ "systemd-cryptsetup@${name}.service" ];
+        after = [ "systemd-cryptsetup@${name}.service" ];
+        wantedBy = [ "multi-user.target" ];
+
+        what = "/dev/mapper/${name}";
+        where = "/mnt/${name}";
+        type = "ntfs3";
+        options = "ro,uid=1000,nofail";
+    }) names;
+in
 
 {
+    environment.etc."crypttab".text = stableLib.concatStringsSep
+        "\n"
+        (stableLib.mapAttrsToList
+            (name: uuid: "${name} UUID=${uuid} /mnt/persist/etc/bitlk-keys/${name} bitlk,nofail")
+        windowsMounts);
+
+    systemd.mounts = mkWindowsMounts (builtins.attrNames windowsMounts);
+
     disko.devices = {
         disk.main = {
             device = "/dev/disk/by-id/nvme-SPCC_M.2_PCIe_SSD_RN001290";
