e7357985fea937f97397e5ec50eb412eb770b5e0
Matthew_Cash <matthew@matthew-cash.com>
2026-06-05 17:44:39 -0700

    home/kvantum: use system accent color

--- home/plasma/kvantum.nix
+++ home/plasma/kvantum.nix
@@ -1,4 +1,14 @@
-{ pkgsUnstable, stableLib, ... }:
+{ pkgsUnstable, stableLib, accentColor, ... }:
+
+let
+    sweetConfig = pkgsUnstable.runCommand "Sweet.kvconfig" {} ''
+        cp ${pkgsUnstable.sweet-nova}/share/Kvantum/Sweet/Sweet.kvconfig $out
+        substituteInPlace $out \
+            --replace-fail "highlight.color=#c50ed2" "highlight.color=${accentColor.hex}" \
+            --replace-fail "inactive.highlight.color=#654ea3" "inactive.highlight.color=${accentColor.hex}" \
+            --replace-fail "link.color=#646464" "link.color=${accentColor.hex}"
+    '';
+in
 
 {
     home.sessionVariables.QT_STYLE_OVERRIDE = stableLib.mkForce "kvantum";
@@ -7,7 +17,8 @@
         General.theme = "Sweet";
     };
 
-    xdg.configFile."Kvantum/Sweet".source = "${pkgsUnstable.sweet-nova}/share/Kvantum/Sweet";
+    xdg.configFile."Kvantum/Sweet/Sweet.kvconfig".source = sweetConfig;
+    xdg.configFile."Kvantum/Sweet/Sweet.svg".source = "${pkgsUnstable.sweet-nova}/share/Kvantum/Sweet/Sweet.svg";
 
     home.packages = with pkgsUnstable; [
         libsForQt5.qtstyleplugin-kvantum
