d7f58d556df292997e2215c62a56367d81f54c9f
Matthew_Cash <matthew@matthew-cash.com>
2026-06-13 08:20:56 -0700

    Update flake inputs

--- nixos/systemd/config.nix
+++ nixos/systemd/config.nix
@@ -4,7 +4,5 @@
     # Limit "A stop job is running..." delay
     systemd.settings.Manager.DefaultTimeoutStopSec = "10s";
 
-    systemd.user.extraConfig = /* ini */ ''
-        DefaultTimeoutStopSec=10s
-    '';
+    systemd.user.settings.Manager.DefaultTimeoutStopSec = "10s";
 }
