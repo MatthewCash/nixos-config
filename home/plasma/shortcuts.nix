2ee94c429a0427a3b2745d266ad9ffdbcfb323b8
Matthew_Cash <matthew@matthew-cash.com>
2026-06-14 22:03:15 -0700

    home/plasma/shortcuts: add shortcuts for audio

--- home/plasma/shortcuts.nix
+++ home/plasma/shortcuts.nix
@@ -1,8 +1,47 @@
-{ ... }:
+{ pkgsUnstable, ... }:
+
+let
+    pactl = "${pkgsUnstable.pulseaudio}/bin/pactl";
+in
 
 {
+    xdg.desktopEntries.plasma-manager-commands.startupNotify = false;
+
     programs.plasma.shortcuts = {
+        kmix = {
+            decrease_volume = [ ];
+            decrease_volume_small = [ ];
+            increase_volume = [ ];
+            increase_volume_small = [ ];
+            mute = [ ];
+        };
         kwin."Window Close" = ["Alt+F4" "Meta+Q"];
         plasmashell."manage activities" = [ ];
+    };
+
+    programs.plasma.hotkeys.commands = {
+        decrease-media-volume = {
+            name = "Decrease Media Volume";
+            comment = "Decrease Media Volume";
+            key = "Volume Down";
+            command = "${pactl} set-sink-volume media -1%";
+            logs.enabled = false;
+        };
+
+        increase-media-volume = {
+            name = "Increase Media Volume";
+            comment = "Increase Media Volume";
+            key = "Volume Up";
+            command = "${pactl} set-sink-volume media +1%";
+            logs.enabled = false;
+        };
+
+        mute-media-volume = {
+            name = "Mute Media Volume";
+            comment = "Mute Media Volume";
+            key = "Volume Mute";
+            command = "${pactl} set-sink-mute media toggle";
+            logs.enabled = false;
+        };
     };
 }
