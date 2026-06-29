4f2455a6e4355a41c33d9bb74cd3b9342a044b54
Matthew_Cash <matthew@matthew-cash.com>
2026-06-28 18:37:05 -0700

    mango/nixos/gpu: limit core clock speed with lact

--- systems/mango/nixos/gpu.nix
+++ systems/mango/nixos/gpu.nix
@@ -1,5 +1,23 @@
 { ... }:
 
 {
-    hardware.amdgpu.initrd.enable = true;
+    hardware.amdgpu = {
+        initrd.enable = true;
+        overdrive.enable = true;
+    };
+
+    services.lact = {
+        enable = true;
+        settings = {
+            version = 5;
+
+            daemon = {
+                log_level = "info";
+                admin_group = "wheel";
+            };
+
+            # this gpu crashes under heavy load :/
+            gpus."1002:744C-1849:5304-0000:03:00.0".max_core_clock = 1700;
+        };
+    };
 }
