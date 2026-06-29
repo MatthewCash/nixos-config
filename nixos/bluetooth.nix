3479820a5b613bbb9a5eaeca12874d3df44a7168
Matthew_Cash <matthew@matthew-cash.com>
2026-06-28 18:37:27 -0700

    nixos/bluetooth: use my ble midi patch

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -1,7 +1,20 @@
-{ persistPath, ... }:
+{ persistPath, pkgsUnstable, ... }:
+
+let
+    bluezMidiZeroTimestampHighPatch = pkgsUnstable.fetchpatch {
+        url = "https://github.com/MatthewCash/bluez/commit/7fbd901a761c4cb163a1d0ca95c9daccade9c889.patch";
+        hash = "sha256-z6QGIK8LVxb4DVN7gqMPtUlW0BeaSyEZXyITebQxgNI=";
+    };
+in
 
 {
     environment.persistence.${persistPath}.directories = [ "/var/lib/bluetooth" ];
 
-    hardware.bluetooth.enable = true;
+    hardware.bluetooth = {
+        enable = true;
+
+        package = pkgsUnstable.bluez.overrideAttrs (oldAttrs: {
+            patches = (oldAttrs.patches or []) ++ [ bluezMidiZeroTimestampHighPatch ];
+        });
+    };
 }
