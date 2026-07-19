f448cd025e8f19913fd916f63262d29a0a925de5
Matthew_Cash <matthew@matthew-cash.com>
2026-07-19 09:56:11 -0700

    nixos/audio: improve stability under load

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -20,18 +20,10 @@
 
         extraConfig.pipewire."92-low-latency" = {
             "context.properties" = {
-                "default.clock.rate" = 48000;  # Fixed rate avoids resampling latency
-                "default.clock.quantum" = 128;      # ~5ms latency at 48kHz
-                "default.clock.min-quantum" = 64;   # ~2.5ms latency at 48kHz
+                "default.clock.rate" = 48000;       # Fixed rate avoids resampling latency
+                "default.clock.quantum" = 128;      # 2.67ms processing period at 48kHz
+                "default.clock.min-quantum" = 64;   # 1.33ms processing period at 48kHz
                 "default.clock.max-quantum" = 512;
-            };
-        };
-
-        extraConfig.pipewire-pulse."92-low-latency" = {
-            "pulse.properties" = {
-                "pulse.min.req" = "64/48000";    # Start with 64, not 32, for stability
-                "pulse.default.req" = "64/48000";
-                "pulse.max.req" = "128/48000";
             };
         };
     };
