1564bb185389feba8c86a55b17be21fee5522bf5
Matthew_Cash <matthew@matthew-cash.com>
2026-07-26 10:35:36 -0700

    nixos/lactd: fix config migration

--- systems/mango/nixos/gpu.nix
+++ systems/mango/nixos/gpu.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgsUnstable, ... }:
 
 {
     hardware.amdgpu = {
@@ -9,7 +9,7 @@
     services.lact = {
         enable = true;
         settings = {
-            version = 5;
+            version = 6;
 
             daemon = {
                 log_level = "info";
@@ -18,6 +18,19 @@
 
             # this gpu crashes under heavy load :/
             gpus."1002:744C-1849:5304-0000:03:00.0".max_core_clock = 1700;
+        };
+    };
+
+    systemd.services.lactd = {
+        environment.LACT_DAEMON_CONFIG_DIR = "/run/lactd-config";
+
+        serviceConfig = {
+            RuntimeDirectory = "lactd-config";
+            ExecStartPre = [
+                "${pkgsUnstable.coreutils}/bin/install -m 0600 /etc/lact/config.yaml /run/lactd-config/config.yaml"
+                "-${pkgsUnstable.coreutils}/bin/rm -f /run/lactd.sock"
+            ];
+            ExecStopPost = "-${pkgsUnstable.coreutils}/bin/rm -f /run/lactd.sock";
         };
     };
 }
