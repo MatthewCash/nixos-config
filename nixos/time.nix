96b5a76a5cb2eec146d6be7aa7803bef239ff9ff
Matthew_Cash <matthew@matthew-cash.com>
2026-08-26 12:04:22 -0700

    nixos/time: persist chrony dir

--- nixos/time.nix
+++ nixos/time.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ persistPath, ... }:
 
 {
     time.timeZone = "America/Los_Angeles";
 
     services.chrony.enable = true;
+
+    environment.persistence.${persistPath}.directories = [{
+        directory = "/var/lib/chrony";
+        user = "chrony";
+        group = "chrony";
+        mode = "0750";
+    }];
 }
