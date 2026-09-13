765608d11205f0c414dacb6543d1af9f2f77a19f
Matthew_Cash <matthew@matthew-cash.com>
2026-09-13 09:36:13 -0700

    nixos/audio: remove low latency profile

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -17,15 +17,6 @@
         enable = true;
         pulse.enable = true;
         jack.enable = true;
-
-        extraConfig.pipewire."92-low-latency" = {
-            "context.properties" = {
-                "default.clock.rate" = 48000;       # Fixed rate avoids resampling latency
-                "default.clock.quantum" = 128;      # 2.67ms processing period at 48kHz
-                "default.clock.min-quantum" = 64;   # 1.33ms processing period at 48kHz
-                "default.clock.max-quantum" = 512;
-            };
-        };
     };
 
     services.pulseaudio.enable = false;
