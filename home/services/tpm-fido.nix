697a8bd0fb7fac7d4cdbb15dd07aed589bed4b30
Matthew_Cash <matthew@matthew-cash.com>
2026-09-22 19:18:56 -0700

    tpm-fido: fix symlink race condition

--- home/services/tpm-fido.nix
+++ home/services/tpm-fido.nix
@@ -1,37 +1,17 @@
-{ pkgsStable, pkgsUnstable, stableLib, config, ... }:
+{ pkgsUnstable, stableLib, config, ... }:
 
 {
-    systemd.user.services = {
-        tpm-fido = {
-            Unit.Description = "tpm-fido service";
-            Service = {
-                ExecStart = stableLib.getExe pkgsUnstable.tpm-fido;
-                Environment = "PATH=${config.home.profileDirectory}/bin"; # to find profile's pinentry
-                Restart = "always";
-            };
-            Install.WantedBy = [ "default.target" ];
+    systemd.user.services.tpm-fido = {
+        Unit = {
+            Description = "tpm-fido service";
+            After = [ "graphical-session-pre.target" ];
+            PartOf = [ "graphical-session.target" ];
         };
-
-        # Service creates a predictable name to pass to sandboxed apps
-        tpm-fido-node = {
-            Unit = {
-                Description = "Create link to tpm-fido hidraw node in XDG_RUNTIME_DIR";
-                After = [ "tpm-fido.service" ];
-                Requires = [ "tpm-fido.service" ];
-            };
-
-            Service = {
-                Type = "oneshot";
-                ExecStart = pkgsStable.writeShellScript "tpm-fido-node" /* bash */ ''
-                    link_path="$XDG_RUNTIME_DIR/tpm-fido-hidrawnode"
-
-                    hidraw_node="$(${stableLib.getExe' pkgsStable.coreutils "basename"} $(ls /sys/bus/hid/devices/0003:15D9:0A37.????/hidraw/ | ${stableLib.getExe pkgsStable.gnused} -n 1p))"
-
-                    ${stableLib.getExe' pkgsStable.coreutils "rm"} -f $link_path
-                    ${stableLib.getExe' pkgsStable.coreutils "ln"} -sf "/dev/$hidraw_node" $link_path
-                '';
-            };
-            Install.WantedBy = [ "default.target" ];
+        Service = {
+            ExecStart = stableLib.getExe pkgsUnstable.tpm-fido;
+            Environment = "PATH=${config.home.profileDirectory}/bin"; # to find profile's pinentry
+            Restart = "always";
         };
+        Install.WantedBy = [ "graphical-session.target" ];
     };
 }
--- nixos/uhid.nix
+++ nixos/uhid.nix
@@ -5,6 +5,7 @@
 
     services.udev.extraRules = ''
         KERNEL=="uhid", SUBSYSTEM=="misc", GROUP="uhid", MODE="0660"
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", KERNELS=="0003:15D9:0A37.*", SYMLINK+="tpm-fido-hidrawnode"
     '';
 
     users.extraUsers = builtins.mapAttrs (name: config: { extraGroups = [ "tss" "uhid" ]; }) users;
